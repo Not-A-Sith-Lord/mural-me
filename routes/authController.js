9db5955762b6399f720e6e6ef2a8ab0e71739408
Not-A-Sith-Lord <arieldeschapell@gmail.com>
2017-02-15 10:25:31 -0500

    Settup basic express app for mural project

---
+++ routes/authController.js
@@ -0,0 +1,48 @@
+const express        = require("express");
+const authController = express.Router();
+
+// User model
+const User           = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+authController.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authController.post("/signup", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "The username already exists" });
+      } else {
+        res.redirect("/login");
+      }
+    });
+  });
+});
+
+module.exports = authController;
